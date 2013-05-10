db7cb235c7611f14ac005dcade84369021e09c98
Carlos Muñoz <carmunrod@gmail.com>
2013-05-10 13:56:55 +0200

    + SQLite3 support.

--- src/spider.py
+++ src/spider.py
@@ -1,20 +1,27 @@
-import os, re, sys; sys.path.insert(0, os.path.join("..", ".."))
+import os, re, sqlite3, sys; sys.path.insert(0, os.path.join("..", ".."))
 from datetime import date
 from pattern.web import Spider, DEPTH, BREADTH, FIFO, LIFO
 
-class SimpleSpider(Spider):
-
+class SQLiteSpider(Spider):
+    
     def visit(self, link, source=None):
         match = re.search("huffingtonpost.co.uk/\d{4}/\d{2}/\d{2}/", link.url)
         if match:
             splitted_url = link.url.split('/')
-            news_date = date(int(splitted_url[3]), int(splitted_url[4]), int(splitted_url[5]))
+            article_date = date(int(splitted_url[3]), int(splitted_url[4]), int(splitted_url[5]))
 #             Por ahora coge el titulo de la url, pero seguramente es mejor cogerlo del HTML
             title = splitted_url[6].split('_')[0]
-            print "Date:", news_date, "\nTitle:", title, "\nUrl:", link.url, "\n\n"
+#             TODO
+            content = ''
+            conn = sqlite3.connect('news.db')
+            c = conn.cursor()
+            c.execute("INSERT INTO news VALUES(?,?,?,?)", (title, str(article_date), link.url, content))
+            conn.commit()
+            conn.close()
+            print "Date:", article_date, "\nTitle:", title, "\nUrl:", link.url, "\n\n"
 
     def fail(self, link):
-        print "failed:", link.url
+        print "failed:", link.url,"\n"
 
     def priority(self, link, method=DEPTH):
         match = re.search("huffingtonpost.co.uk/\d{4}/\d{2}/\d{2}/", link.url)
@@ -24,7 +31,13 @@ class SimpleSpider(Spider):
             return 0.0
 
 
-spider = SimpleSpider(links=["http://www.huffingtonpost.co.uk/"], domains=["huffingtonpost.co.uk"], delay=0.0)
+conn = sqlite3.connect('news.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS news (title text, date text, url text, content text)''')
+conn.commit()
+conn.close()
+
+spider = SQLiteSpider(links=["http://www.huffingtonpost.co.uk/"], domains=["huffingtonpost.co.uk"], delay=0.0)
 
 while True:
     spider.crawl(cached=False)
